9c6bf92ab74b10973942fdd7bf0cae4a46a931bd
SURYA IDURI <tejs4100gmail.com>
2022-11-09 11:32:14 +0530

    first commit

---
+++ call.js
@@ -0,0 +1,55 @@
+
+// 1 .Write a function and use 'call' to call the function
+    const person = {
+    fullName: function(city, country) {
+      return this.firstName + " " + this.lastName + "," + city + "," + country;
+    }
+  }
+  
+  const person1 = {
+    firstName:"John",
+    lastName: "Doe"
+  }
+  
+  person.fullName.call(person1, "Oslo", "Norway");
+
+
+
+  // 2 Write a program using apply
+  const person = {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  
+  const person1 = {
+    firstName: "Mary",
+    lastName: "Doe"
+  }
+  
+  // This will return "Mary Doe":
+  person.fullName.apply(person1);
+
+  const person = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  
+  const member = {
+    firstName:"Hege",
+    lastName: "Nilsen",
+  }
+  
+  let fullName = person.fullName.bind(member);
+
+
+  function student(name, class, rollno)
+{ 
+this.name = name 
+this.class = class
+this.class = rollno
+this.studentDetails = studentDetails
+} 
